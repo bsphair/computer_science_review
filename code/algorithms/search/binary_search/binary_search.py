ac6ccbf3fe5c385860f261be15945abc6b3b1438
bsphair <bsphair@gmail.com>
2020-04-19 22:30:51 -0400

    adding iterative and recursive binary search in python

--- code/algorithms/search/binary_search/binary_search.py
+++ code/algorithms/search/binary_search/binary_search.py
@@ -1,7 +1,7 @@
 averages = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
-def findNum(data, num):
+def binary_search_interative(data, num):
     low = data[0]
     high = data[len(data) - 1]
     
@@ -16,7 +16,19 @@ def findNum(data, num):
     
     return 'Number does not exist'
     
+    
+def binary_search_recursive(data, low, high, num):
+    if low <= high:
+        mid = (high + low) // 2
+        if num == mid:
+            return 'found'
+        elif num > mid:
+            return binary_search_recursive(data, mid + 1, high, num)
+        elif num < mid:
+            return binary_search_recursive(data, low, mid - 1, num)
+    else:
+        return 'Number does not exist'
 
+print(binary_search_interative(averages, 1))
+print(binary_search_recursive(averages, 0, len(averages) - 0, 1))
 
-pos = findNum(averages, 1)
-print(pos)
